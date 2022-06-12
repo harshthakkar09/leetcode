445a7f72ac792356d0900092d3470cbc1121a715
harshthakkar09 <42773660+harshthakkar09@users.noreply.github.com>
2022-06-12 20:32:04 +0530

    Time: 18 ms (75.00%), Space: 13.4 MB (50.00%) - LeetHub

---
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double ans=0.0;
+        int n=brackets.size(),i=0,pre=0;
+        while(i<n){
+            int earning=brackets[i][0];
+            int percent=brackets[i][1];
+            ans+=(min(earning,income)-pre)*((double)percent/100.0);
+            // cout<<"earning "<<earning<<" percent "<<percent<<"\n";
+            pre=earning;
+            if(earning>=income){
+                return ans;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
