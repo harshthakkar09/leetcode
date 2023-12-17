9bee3c69b0fa05472818dcfb65b2da88a76eabab
harshthakkar09 <42773660+harshthakkar09@users.noreply.github.com>
2023-12-17 11:41:51 +0530

    Time: 367 ms (90.43%), Space: 162.6 MB (79.90%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -1,32 +1,51 @@
+typedef pair<int, string> pis;
+
+class Compare {
+    public:
+       bool operator()(pis a, pis b){
+           if(a.first == b.first){
+               return a.second > b.second;
+           }
+           else{
+               return a.first < b.first;
+           }
+      }
+};
+
 class FoodRatings {
 public:
     
-    map<string, set<pair<int, string>>> cuisine_items;
-    unordered_map<string, string> food_cuisine;
-    unordered_map<string, int> food_rating;
+    unordered_map<string, int> foodToRating;
+    unordered_map<string, string> foodToCuisine;
+    unordered_map<string, priority_queue<pis, vector<pis>, Compare>> cuisineToFoodRating;
     
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         int n = foods.size();
+        
         for(int i = 0; i < n; i++){
             string food = foods[i], cuisine = cuisines[i];
             int rating = ratings[i];
-            cuisine_items[cuisine].insert({-rating, food});
-            food_cuisine[food] = cuisine;
-            food_rating[food] = -rating;
+            foodToRating[food] = rating;
+            foodToCuisine[food] = cuisine;
+            cuisineToFoodRating[cuisine].push({rating, food});
         }
     }
     
     void changeRating(string food, int newRating) {
-        string cuisine = food_cuisine[food];
-        int rating = food_rating[food];
-        cuisine_items[cuisine].erase({rating, food});
-        cuisine_items[cuisine].insert({-newRating, food});
-        food_rating[food] = -newRating;
+        foodToRating[food] = newRating;
+        string cuisine = foodToCuisine[food];
+        cuisineToFoodRating[cuisine].push({newRating, food});
     }
     
     string highestRated(string cuisine) {
-        pair<int, string> selected_food = *(cuisine_items[cuisine].begin());
-        return selected_food.second;
+        auto highRatedFood = cuisineToFoodRating[cuisine].top();
+        
+        while(foodToRating[highRatedFood.second] != highRatedFood.first){
+            cuisineToFoodRating[cuisine].pop();
+            highRatedFood = cuisineToFoodRating[cuisine].top();
+        }
+        
+        return highRatedFood.second;
     }
 };
 
