3cdbf7c12fdfe3d5a4cf48dad5f4d59ded54183b
harshthakkar09 <42773660+harshthakkar09@users.noreply.github.com>
2024-03-06 08:02:31 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (33.47%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(auto c : s){
+            if(c == '('){
+                st.push(c);
+            }
+            else{
+                if(!st.empty() && st.top() == '('){
+                    st.pop();
+                }
+                else{
+                    st.push(c);
+                }
+            }
+        }
+        return st.size();
+    }
+};
